8de28c2724cbb9ea99e42960deb5ca93620019d2
Faleye Emmanuel <efaleye3@gmail.com>
2025-09-03 19:26:27 +0100

    Update password.js

--- password.js
+++ password.js
@@ -28,4 +28,20 @@ copyIcon.addEventListener('click', () => {
         alert("Password copied to clipboard!");
     });   
 });
+
+eyeIcon.onclick = function(){
     
+    if(password.type === "password"){
+        password.type = "text";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }else{
+        password.type = "password";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    }
+
+}
+    
+    
+
